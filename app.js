d4fc6da9446067391c44cb8d2ef88b314da7a9df
zaki <zaki.ilhami46@gmail.com>
2023-02-20 14:23:54 +0700

    add

---
+++ app.js
@@ -0,0 +1,26 @@
+// var name = "zaki";
+// var greeting = "hello,";
+// console.log(greeting + name);
+
+// var and let
+// hoisting
+// console.log(name);
+
+//scope
+// for (let i = 0; i < 3; i++) {
+//     console.log("hello" + i);
+// }
+// console.log(i)
+
+// function foo() {
+//     var fooVar = "bar";
+//     console.log("didalam function" + fooVar);
+//     return fooVar;
+// }
+// foo();
+// console.log(fooVar);
+let name = "zaki";
+var foo = "bar";
+const title = document.querySelector("#title");
+// heading.innerHTML = "Hello World";
+title.textContent = "Update title from js file";
